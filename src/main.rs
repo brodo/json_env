c91debdf9edd7fdf955ae93174d776e3beb5a185
Julian Dax <julian.dax@posteo.de>
2022-09-30 21:58:24 +0200

    chore: fix formatting

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use anyhow::Result;
 use std::collections::HashMap;
 use std::env;
 use std::fs::File;
 use std::io::Read;
 use std::process::Command;
 
+use anyhow::Result;
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 
