f1acc6edb0557d4e6946ed03c063e07e5619f0ef
Julian Dax <julian.dax@posteo.de>
2022-09-30 21:32:21 +0200

    fix: other error in npm install script

--- npm/getBinary.js
+++ npm/getBinary.js
@@ -25,9 +25,7 @@ function getPlatform() {
         if(arch === 'arm64')  {
             return 'aarch64-macos';
         }
-
     }
-
     throw new Error(`Unsupported platform: ${type} ${arch}. Please create an issue at https://github.com/woubuc/sweep/issues`);
 }
 
@@ -36,7 +34,7 @@ function getBinary() {
     const version = require('../package.json').version;
     const extension = platform === 'x86_64-windows' ? 'zip' : 'tar.xz';
     const url = `https://github.com/brodo/json_env/releases/download/v${version}/json_env-v${version}-${platform}.${extension}`;
-    return new Binary(url, {name: 'json_env'});
+    return new Binary('json_env', url);
 }
 
 module.exports = getBinary;
